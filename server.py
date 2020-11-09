2b80de85a21f9ea30ccf42c03eafd186be5e9b41
Laura Docherty <laurahdocherty@gmail.com>
2020-11-09 14:02:39 -0600

    sudoku board game logic complete

--- server.py
+++ server.py
@@ -72,6 +72,40 @@ class SudokuGame(object):
             for j in xrange(9):
                 self.puzzle[i].append(self.start_puzzle[i][j])
 
+    def check_win(self):
+        for row in xrange(9):
+            if not self.__check_row():
+                return False
+        for colum in xrange(9):
+            if not self.__check_column():
+                return False
+        for row in xrange(3):
+            for column in xrange(3):
+                if not self.__check_square(row, column):
+                    return False
+        self.game_over = True
+        return True
+
+    def __check_block(self, block):
+        return set(block) == set(range(1,10))
+
+    def __check_row(self, row):
+        return self.__check_block(self.puzzle[row])
+
+    def __check_column(self, column):
+        return self.__check_block(
+            [self.puzzle[row][column] for row in xrange(9)]
+        )
+
+    def __check_square(self, row, column):
+        return self.__check_block(
+            [
+                self.puzzle[r][c]
+                for r in xrange(row * 3, (row + 1) * 3)
+                for c in xrange(column * 3, (column + 1) * 3)
+            ]
+        )
+
 
 
 
